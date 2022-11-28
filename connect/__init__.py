05b0a70609837206bad702b64782c92b0b08025e
Ian Eisenberg <ian@credo.ai>
2022-11-28 14:17:54 -0800

    added version validation

--- connect/__init__.py
+++ connect/__init__.py
@@ -1,7 +1,10 @@
 """
 Credo AI Connect package
 """
+from connect.utils.version_check import validate_version
 
 __version__ = "0.0.4"
 
 __all__ = ["governance", "evidence", "utils"]
+
+validate_version()
---
+++ connect/utils/version_check.py
@@ -0,0 +1,24 @@
+import requests
+
+import connect
+from connect.utils import global_logger
+
+
+def validate_version():
+    current_version = connect.__version__
+
+    package = "credoai-connect"
+    response = requests.get(f"https://pypi.org/pypi/{package}/json")
+    latest_version = response.json()["info"]["version"]
+
+    on_latest = current_version == latest_version
+
+    if not on_latest:
+        global_logger.warning(
+            """
+            You are using credoai-connect version %s, however a newer version is available.
+            Lens is updated regularly with major improvements and bug fixes.
+            Please upgrade via the command: "python -m pip install --upgrade credoai-connect"
+            """,
+            current_version,
+        )
