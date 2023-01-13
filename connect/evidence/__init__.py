99330fbab8266adc8fbf3efa8b1695857a8994fe
fabrizio-credo <fabrizio@credo.ai>
2023-01-12 17:13:54 -0800

    Updated imports

--- connect/evidence/__init__.py
+++ connect/evidence/__init__.py
@@ -4,6 +4,11 @@ Everything needed to generate inputs for the Credo AI Platform
 All the logic defined here is in order to standardize communication with 
 Credo AI Platform API
 """
-from .containers import EvidenceContainer, MetricContainer, TableContainer
-from .evidence import Evidence, MetricEvidence, TableEvidence
+from .containers import (
+    EvidenceContainer,
+    MetricContainer,
+    TableContainer,
+    StatisticTestContainer,
+)
+from .evidence import Evidence, MetricEvidence, TableEvidence, StatisticTestEvidence
 from .evidence_requirement import EvidenceRequirement
